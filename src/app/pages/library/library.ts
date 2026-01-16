95ea7e0fb8dc34254847b49f77c3ad3865780b84
mohamedelasri202 <el7994995@gmail.com>
2026-01-16 09:44:55 +0100

    Implement Spotify-like filter menu and add form toggle logic

--- src/app/pages/library/library.ts
+++ src/app/pages/library/library.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AddTrack } from '../add-track/add-track';
 import { TrackService } from '../../services/track-service';
 import { StorageService } from '../../services/storage-service';
@@ -9,19 +9,27 @@ import { Button } from '../../shared/components/button/button';
 import { CommonModule } from '@angular/common';
 import { AudioPlayerService } from '../../services/audio-player-service';
 import { RouterModule } from '@angular/router';
+import { FilterMenu } from '../../shared/components/filter-menu/filter-menu';
 
 @Component({
   selector: 'app-library',
-  imports: [AddTrack, CommonModule, RouterModule],
+  imports: [AddTrack, CommonModule, RouterModule, FilterMenu],
   templateUrl: './library.html',
   styleUrl: './library.css',
 })
 export class Library {
   trackService = inject(TrackService)
   private audioService = inject(AudioPlayerService);
+  isMenuOpen = signal(false)
+  currentSort = 'Date Added';
   tracks = this.trackService.tracks
   selectedTrack: Track | null = null;
   isFormVisible: boolean = false;
+
+  onSortChange(option: string) {
+    this.currentSort = option;
+    this.isMenuOpen.set(false);
+  }
 
   deleteTrack(id: number | undefined) {
     console.log('Delete button clicked for ID:', id);
@@ -45,6 +53,11 @@ export class Library {
   playTrack(track: Track) {
     this.audioService.loadTrack(track);
   }
+
+  toggleFilterMenu() {
+    this.isMenuOpen.update(value => !value);
+  }
+
 
 
 }
---
+++ src/app/shared/components/filter-menu/filter-menu.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-filter-menu',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './filter-menu.html',
+  styleUrl: './filter-menu.css',
+})
+export class FilterMenu {
+  @Input() selectedOption: string = '';
+  @Output() sortSelected = new EventEmitter<string>();
+
+  // "Album" is excluded as requested
+  options = ['Title', 'Artist', 'Date Added'];
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.sortSelected.emit(option);
+  }
+}
